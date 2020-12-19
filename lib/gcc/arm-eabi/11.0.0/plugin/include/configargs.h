0d9bda920899df3526bd9297b4918da79b105e8c
mvaisakh <vaisakhmurali@gmail.com>
2020-12-19 07:31:01 +0000

    Import GCC 20201219

---
+++ lib/gcc/arm-eabi/11.0.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi --prefix=/home/runner/work/build-tools-gcc/build-tools-gcc/../gcc-arm --disable-nls --disable-shared --disable-docs --enable-languages=c,c++ --without-headers";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "arm7tdmi" } };
