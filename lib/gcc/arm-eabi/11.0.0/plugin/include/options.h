1c154a77642ae03f6dd606c884b385447173ca74
mvaisakh <vaisakhmurali@gmail.com>
2021-02-11 06:35:42 +0530

    Import ARM GCC 20210211
    Build completed on: Thu Feb 11 06:35:42 IST 2021
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-eabi-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm/bin/../libexec/gcc/arm-eabi/11.0.0/lto-wrapper
    Target: arm-eabi
    Configured with: ../gcc/configure --target=arm-eabi --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm --disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-sysroot
    Thread model: single
    Supported LTO compression algorithms: zlib
    gcc version 11.0.0 20210210 (experimental) (Eva GCC)
    
    Signed-off-by: mvaisakh <vaisakhmurali@gmail.com>

--- lib/gcc/arm-eabi/11.0.0/plugin/include/options.h
+++ lib/gcc/arm-eabi/11.0.0/plugin/include/options.h
@@ -17,18 +17,6 @@ struct gcc_options
 {
 #endif
 #ifdef GENERATOR_FILE
-extern HOST_WIDE_INT function_entry_patch_area_size;
-#else
-  HOST_WIDE_INT x_function_entry_patch_area_size;
-#define function_entry_patch_area_size global_options.x_function_entry_patch_area_size
-#endif
-#ifdef GENERATOR_FILE
-extern HOST_WIDE_INT function_entry_patch_area_start;
-#else
-  HOST_WIDE_INT x_function_entry_patch_area_start;
-#define function_entry_patch_area_start global_options.x_function_entry_patch_area_start
-#endif
-#ifdef GENERATOR_FILE
 extern bool dump_base_name_prefixed;
 #else
   bool x_dump_base_name_prefixed;
@@ -5861,6 +5849,12 @@ extern int flag_partial_inlining;
 #define flag_partial_inlining global_options.x_flag_partial_inlining
 #endif
 #ifdef GENERATOR_FILE
+extern const char *flag_patchable_function_entry;
+#else
+  const char *x_flag_patchable_function_entry;
+#define flag_patchable_function_entry global_options.x_flag_patchable_function_entry
+#endif
+#ifdef GENERATOR_FILE
 extern int flag_pcc_struct_return;
 #else
   int x_flag_pcc_struct_return;
@@ -7607,6 +7601,7 @@ struct GTY(()) cl_optimization
   const char *x_str_align_jumps;
   const char *x_str_align_labels;
   const char *x_str_align_loops;
+  const char *x_flag_patchable_function_entry;
   int x_param_align_loop_iterations;
   int x_param_align_threshold;
   int x_param_asan_protect_allocas;
@@ -8072,7 +8067,7 @@ struct GTY(()) cl_optimization
   signed char x_flag_web;
   signed char x_flag_wrapv;
   signed char x_flag_wrapv_pointer;
-  /* 469 members */
+  /* 470 members */
   unsigned HOST_WIDE_INT explicit_mask[8];
 };
 
