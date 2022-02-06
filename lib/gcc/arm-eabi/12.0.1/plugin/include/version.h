a02dabbad3f19869eb2755e216834324e55f37a9
mvaisakh <mvaisakh@statixos.com>
2022-02-06 12:06:28 +0530

    Import ARM GCC 20220206
    Build completed on: Sun Feb  6 12:06:28 IST 2022
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-eabi-gcc
    COLLECT_LTO_WRAPPER=/home/runner/work/gcc-build/gcc-arm/bin/../libexec/gcc/arm-eabi/12.0.1/lto-wrapper
    Target: arm-eabi
    Configured with: ../gcc/configure --target=arm-eabi --prefix=/home/runner/work/gcc-build/gcc-build/../gcc-arm --disable-decimal-float --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --disable-docs --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --with-pkgversion='Eva GCC' --with-newlib --with-gnu-as --with-gnu-ld --with-linker-hash-style=gnu --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib
    gcc version 12.0.1 20220206 (Bleeding Edge) (Eva GCC)
    
    LLD: LLD 15.0.0 (compatible with GNU linkers)
    Signed-off-by: mvaisakh <mvaisakh@statixos.com>

--- lib/gcc/arm-eabi/12.0.1/plugin/include/version.h
+++ lib/gcc/arm-eabi/12.0.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.0.1 20220203 (Bleeding Edge)"
+#define version_string "12.0.1 20220206 (Bleeding Edge)"
 #define pkgversion_string "(Eva GCC) "
 
 /* This is the location of the online document giving instructions for
