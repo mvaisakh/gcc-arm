392dd85f5210570456fa957711a75c8dc5d4695f
mvaisakh <mvaisakh@statixos.com>
2023-02-08 01:51:59 +0530

    Import ARM GCC 20230207
    Build completed on: Wed Feb  8 01:51:59 IST 2023
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-eabi-gcc
    COLLECT_LTO_WRAPPER=/__w/gcc-build/gcc-arm/bin/../libexec/gcc/arm-eabi/13.0.1/lto-wrapper
    Target: arm-eabi
    Configured with: ../gcc/configure --target=arm-eabi --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 13.0.1 20230207 (Bleeding Edge) (Eva GCC)
    
    LLD: LLD 17.0.0 (compatible with GNU linkers)
    Signed-off-by: mvaisakh <mvaisakh@statixos.com>

--- lib/gcc/arm-eabi/13.0.1/plugin/include/configargs.h
+++ lib/gcc/arm-eabi/13.0.1/plugin/include/configargs.h
@@ -1,5 +1,5 @@
 /* Generated automatically. */
-static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi CFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' CXXFLAGS='-flto -flto-compression-level=10 -O3 -pipe -ffunction-sections -fdata-sections' --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot";
+static const char configuration_arguments[] = "../gcc/configure --target=arm-eabi --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot";
 static const char thread_model[] = "posix";
 
 static const struct {
