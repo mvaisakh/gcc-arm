d6dc7645e1f5c064a150a513db4c2630420a2685
mvaisakh <mvaisakh@statixos.com>
2023-04-06 02:40:44 +0530

    Import ARM GCC 20230405
    Build completed on: Thu Apr  6 02:40:44 IST 2023
    
    Configuration: Using built-in specs.
    COLLECT_GCC=./bin/arm-eabi-gcc
    COLLECT_LTO_WRAPPER=/__w/gcc-build/gcc-arm/bin/../libexec/gcc/arm-eabi/13.0.1/lto-wrapper
    Target: arm-eabi
    Configured with: ../gcc/configure --target=arm-eabi --disable-decimal-float --disable-docs --disable-gcov --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath --disable-libstdcxx-pch --disable-nls --disable-shared --enable-default-ssp --enable-languages=c,c++ --enable-threads=posix --prefix=/__w/gcc-build/gcc-build/../gcc-arm --with-gnu-as --with-gnu-ld --with-headers=/usr/include --with-linker-hash-style=gnu --with-newlib --with-pkgversion='Eva GCC' --with-sysroot
    Thread model: posix
    Supported LTO compression algorithms: zlib zstd
    gcc version 13.0.1 20230405 (Bleeding Edge) (Eva GCC)
    
    LLD: LLD 17.0.0 (compatible with GNU linkers)
    Signed-off-by: mvaisakh <mvaisakh@statixos.com>

--- lib/gcc/arm-eabi/13.0.1/plugin/include/plugin-version.h
+++ lib/gcc/arm-eabi/13.0.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "13.0.1";
-static char datestamp[] = "20230402";
+static char datestamp[] = "20230405";
 static char devphase[] = "Bleeding Edge";
 static char revision[] = "";
 
